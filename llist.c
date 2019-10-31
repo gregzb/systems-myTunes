b3d7313d6755109a5735945366b53240245ae69f
Greg Zborovsky <greg.zbo3@gmail.com>
2019-10-31 10:38:50 -0400

    boop #2

--- llist.c
+++ llist.c
@@ -22,10 +22,7 @@ void print_list(struct node * list) {
 }
 
 struct node * insert_front(struct node * list, char * name, char * artist) {
-//gen node
-    struct node * new_node = malloc(sizeof(struct node));
-    strncpy(new_node->name, name, sizeof(new_node->name));
-    strncpy(new_node->artist, artist, sizeof(new_node->artist));
+    struct node * new_node = gen_node(name, artist);
     new_node->next = list;
     return new_node;
 }
@@ -66,10 +63,19 @@ struct node * remove_node(struct node *front, char * name, char * artist) {
 }
 
 struct node * insert_ordered(struct node * front, char * name, char * artist) {
-//gen node
-    struct node * new_node = malloc(sizeof(struct node));
-    strncpy(new_node->name, name, sizeof(new_node->name));
-    strncpy(new_node->artist, artist, sizeof(new_node->artist));
+    struct node * new_node = gen_node(name, artist);
+
+    char found = 0;
+
+    struct node * current = front;
+    while (current != NULL ) {
+        if (strcmp(new_node->artist, current->artist) > 0 || strcmp(new_node->artist, current->artist) == 0 && strcmp(new_node->name, current->name) > 0) {
+            found = 1;
+            break;
+        }
+        current -> next;
+    }
+    if (found)
 }
 
 struct node * find_node(struct node * front, char * name, char * artist) {
